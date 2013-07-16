afe6f81563aa017c1238718e6c99b387379430f4
Captain Capslock <spam+github@captaincapslock.com>
2013-07-16 04:05:56 +0200

    Added minified Javascript code for bookmarklet

---
+++ minified.js
@@ -0,0 +1,23 @@
+// 300px, resizing h1, h2, h3, h4, h5, h6, p, ul, li and blockquote
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote".split(" ");s="300px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
+
+// 400px, resizing h1, h2, h3, h4, h5, h6, p, ul, li and blockquote
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote".split(" ");s="400px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
+
+// 500px, resizing h1, h2, h3, h4, h5, h6, p, ul, li and blockquote
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote".split(" ");s="500px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
+
+// 600px, resizing h1, h2, h3, h4, h5, h6, p, ul, li and blockquote
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote".split(" ");s="600px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
+
+// 300px, resizing h1, h2, h3, h4, h5, h6, p, ul, li, blockquote and images
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote img".split(" ");s="300px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
+
+// 400px, resizing h1, h2, h3, h4, h5, h6, p, ul, li, blockquote and images
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote img".split(" ");s="400px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
+
+// 500px, resizing h1, h2, h3, h4, h5, h6, p, ul, li, blockquote and images
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote img".split(" ");s="500px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
+
+// 600px, resizing h1, h2, h3, h4, h5, h6, p, ul, li, blockquote and images
+javascript:(function(){e="h1 h2 h3 h4 h5 h6 p ul li blockquote img".split(" ");s="600px";for(var c in e){var a=document.getElementsByTagName(e[c]),b;for(b in a)"object"===typeof a[b]&&(a[b].style.maxWidth=s,a[b].style.textAlign="justify")}})();
